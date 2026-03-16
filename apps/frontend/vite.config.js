ebe756285b690301f8541817cd5e33906dcaf95d
JavEstrada09 <javierdeleon200418@gmail.com>
2026-03-16 11:46:46 -0600

    fix: allow external hosts in vite config

--- apps/frontend/vite.config.js
+++ apps/frontend/vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   preview: {
-    host: true,
-    allowedHosts: "all"
-  }
+    allowedHosts: ['all'],
+  },
+  server: {
+    allowedHosts: ['all'],
+  },
 })
