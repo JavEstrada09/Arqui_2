9b68a2282cbd1a87120531af452d8144c480f0d5
JavEstrada09 <javierdeleon200418@gmail.com>
2026-04-13 20:50:57 -0600

    fix port railway

--- server.js
+++ server.js
@@ -54,7 +54,6 @@ const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
 app.get("/", (req, res) => {
   res.send("API funcionando 🚀 usa /graphql");
 });
